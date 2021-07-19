2faff2f38dfdcbf6c34c01ffb11d39d0c0e5a81c
jakub.gortych <jakub.gortych@huuugegames.com>
2021-07-19 21:50:33 +0200

    Temporarily disable bricks while hitting them.

--- Assets/BricksScript.cs
+++ Assets/BricksScript.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class BricksScript : MonoBehaviour
 {
+    private Collider2D rigidbody2d;
+
+    private const uint disabilityTime = 4;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.rigidbody2d = gameObject.GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -18,6 +22,17 @@ public class BricksScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        StartCoroutine("HandleCollision");
+    }
+
+    private IEnumerator HandleCollision()
+    {
+        gameObject.GetComponent<Renderer>().enabled = false;
+        this.rigidbody2d.enabled = false;
+
+        yield return new WaitForSeconds(disabilityTime);
+
+        gameObject.GetComponent<Renderer>().enabled = true;
+        this.rigidbody2d.enabled = true;
     }
 }
